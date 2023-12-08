26eabeac672fa7e51129e92f4463840d379b8a3b
ZeoxEmz <ezequielcaceres98@outlook.com>
2023-12-08 16:00:16 -0300

    access all

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ server.use((req, res, next) => {
     res.header('Content-Type', 'application/json; charset=utf-8');
 
     // Permite el acceso desde el dominio específico
-    res.header('Access-Control-Allow-Origin','https://zeoxemz.github.io, https://zeoxemz.github.io/podex/home');
+    res.header('Access-Control-Allow-Origin','*');
 
     // Permite el acceso de credenciales en las solicitudes
     res.header('Access-Control-Allow-Credentials', 'true');
